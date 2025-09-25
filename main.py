42f61484fada0e7fc81b6bb0559fc3447dff4310
MUNOZ LEMUS SEBASTIAN <smunoz23@cuc.edu.co>
2025-09-25 18:13:48 -0500

    agregar funcion dividir

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ class operaciones:
         return self._a + self._b
     def resta(self):
         return self._a - self._b
+    def division(self):
+        if self._b == 0:
+            return "error: division por cero"
+        return self._a / self._b
 if __name__ == "__main__":
     op = operaciones(5, 3)
     print("Suma:", op.suma())
     print("Resta:", op.resta())
+
+
+
+
