e3a3c5b9d398fa232a46bbc41ca1662466b52a33
mohamedpelea23-dev <mohamedpelea23@gmail.com>
2025-09-25 16:00:00 -0500

    Agregar clase operaciones con método suma

---
+++ main.py
@@ -0,0 +1,9 @@
+class operaciones:
+    def __init__(self,a,b):
+        self._a=a
+        self._b=b 
+    def suma(self):
+        return self._a + self._b
+if __name__ == "__main__":
+    op = operaciones(5, 3)
+    print("Suma:", op.suma())
